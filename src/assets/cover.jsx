4338a6aabe987d19353fd51c26a92c4fe88dc758
Mario Youssef <marioyoussef936@gmail.com>
2024-01-15 14:54:37 +0200

    last

--- src/assets/cover.jsx
+++ src/assets/cover.jsx
@@ -5,10 +5,9 @@ function Cover(){
     return (
         <div className="cont">
           <div className="centerit">
-            <div className="bright">
-        
+            <div className="bright">   
           <h1 className="name" >RIAD KATRIB</h1>
-          <p>Helping People Live Happier, Healthier Lives<br/>Through Improved Fitness, Nutrition And Mindset.</p>
+          <p className="">Helping People Live Happier, Healthier Lives<br/>Through Improved Fitness, Nutrition And Mindset.</p>
           <a className="contactme" href="#Contact">Contact Me</a>
         
 </div>
