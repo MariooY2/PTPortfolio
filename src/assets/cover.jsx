4fdbb122c5be39947b1e915927c5718693e8790a
Mario Youssef <marioyoussef936@gmail.com>
2024-01-15 13:51:07 +0200

    last changes

--- src/assets/cover.jsx
+++ src/assets/cover.jsx
@@ -6,9 +6,11 @@ function Cover(){
         <div className="cont">
           <div className="centerit">
             <div className="bright">
+        
           <h1 className="name" >RIAD KATRIB</h1>
           <p>Helping People Live Happier, Healthier Lives<br/>Through Improved Fitness, Nutrition And Mindset.</p>
           <a className="contactme" href="#Contact">Contact Me</a>
+        
 </div>
 </div>
         </div>
