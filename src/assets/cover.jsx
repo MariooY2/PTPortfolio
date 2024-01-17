543d61b5227719bacd62541860b94ddfc99156b8
Mario Youssef <marioyoussef936@gmail.com>
2024-01-17 16:24:56 +0200

    small changes

--- src/assets/cover.jsx
+++ src/assets/cover.jsx
@@ -7,7 +7,7 @@ function Cover(){
           <div className="centerit">
             <div className="bright">   
           <h2 className="name" >Welcome to RK Fitness</h2>
-          <p className="subheading1">one day or day.<span className="subheading2">one you decide</span></p>
+          <p className="subheading1">one day <span className="subheading2">or</span>day one<span className="subheading2">you decide</span></p>
           <a className="contactme" href="#Contact">Contact Me</a>
         
 </div>
